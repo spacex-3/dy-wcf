8ce9efe0c9fe9dc9c755c638ffca9f5c7ae8b266
fantasysk <lisankai93@qq.com>
2025-01-13 10:12:01 +0800

    v1

--- douyin.py
+++ douyin.py
@@ -9,19 +9,17 @@ from datetime import datetime
 from plugins import register, Plugin, Event, logger, Reply, ReplyType
 
 '''
-  "plugins": [
-    {
-      "name": "douyin",
-      "command": ["复制打开抖音", "v.douyin", "douyin.com", "tiktok.com", "kuaishou.com"],
-      "limit_size": 50,      # 限制视频大小，单位MB
-      "without_at": {        # 无需@机器人也会解析，bool或dict
-        "wx_userid": true,     # 私聊
-        "xxxx@chatroom": true, # 群聊
-        "*": false
-      },
-      "keep_assets_days": 3              # 清理N天前的视频缓存
-    }
-  ]
+plugins:
+
+  - name: douyin
+    command: [复制打开抖音, v.douyin, douyin.com]
+    limit_size: 50  # 限制视频大小，单位MB
+    without_at:     # 无需@机器人也会解析，bool或dict
+      wx_userid: true,     # 私聊
+      xxxx@chatroom: true, # 群聊
+      *: false
+    keep_assets_days: 1
+
 '''
 
 @register
